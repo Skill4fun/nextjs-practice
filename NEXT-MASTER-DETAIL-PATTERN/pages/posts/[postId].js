02dde0f9fd9a0e2e5384ab36a41bffbc3d4a3232
István Gergely Almási <almasi.istvan.gergely@gmail.com>
2023-08-13 19:01:07 +0200

    notes for fallback key

--- NEXT-MASTER-DETAIL-PATTERN/pages/posts/[postId].js
+++ NEXT-MASTER-DETAIL-PATTERN/pages/posts/[postId].js
@@ -13,8 +13,10 @@ const Post = ({ post }) => {
 export default Post;
 
 
-//getStaticPaths to inform nextjs of the possible values that postId.js page should be staticly generated for
+//getStaticPaths is to inform nextjs of the possible values that postId.js page should be staticly generated for
 export async function getStaticPaths() {
+  //dinamically fetch all the post IDs (100pcs)
+
   const response = await fetch(`http://jsonplaceholder.typicode.com/posts`)
   const data = await response.json();
   const paths = data.map(post => {
@@ -28,28 +30,43 @@ export async function getStaticPaths() {
   return {
     //hardcoded paths object:
     /*
-    paths: [
-      {
-        params: { postId: '1' }
-      },
-      {
-        params: { postId: '2' }
-      },
-      {
-        params: { postId: '3' }
-      },
-    ],
-    fallback: false,
-  }
+  
+      paths: [
+        {
+          params: { postId: '1' }
+        },
+        {
+          params: { postId: '2' }
+        },
+        {
+          params: { postId: '3' }
+        },
+      ],
+      fallback: true,
+    }
   */
 
     //dinamically generated paths object:
     paths: paths,
-    fallback: false,
-
+    fallback: false, //fallback key is mandatory, can be: false/true/'blocking'
   }
-
 }
+
+////fallback key set to...:
+//- FALSE:
+// 1. The paths returned from getStaticPaths will be rendered to HTML at build time by getStaticProps.
+// 2. If fallback is set to false, then any paths not returned by getStaticPaths will result in 404 page.
+// 3. When to use: 
+// The false value is most suitable if you have an appllication with a small number of paths to pre - render.
+// When new pages are not added often.
+// A blog site with a few articles is a good example
+//- TRUE:
+// 1. The paths returned from getStaticPaths will be rendered to HTML at build time by getStaticProps.
+// 2. The paths that have not been generated at build time will not result in a 404 page. Instead, Next.js will serve a "fallback" version of the page on the first request to such a path
+// 3. In the background, Next.js will statically generate the requested path HTML and JSON. This includes running getStaticProps.
+// 4. When that's done, the browser receives the JSON for the generated path. This will be used to automatically render the page with the required props. From the user perspective, the page will be swapped from the fallback page to the full page.
+// 5. At the same time, Next.js keeps track of the new list of pre-rendered pages. Subsequent requests to the same path will serve the generated page, just like other pages pre-rendered at build time.
+
 
 
 //context paramater is an object which contains a key called params, 
@@ -59,6 +76,10 @@ export async function getStaticProps(context) {
 
   const response = await fetch(`http://jsonplaceholder.typicode.com/posts/${params.postId}`);
   const data = await response.json();
+
+
+
+  console.log(`Gererating page for /posts/${params.postId}`);
 
   return {
     props: {
--- NEXT-MASTER-DETAIL-PATTERN/pages/posts/index.js
+++ NEXT-MASTER-DETAIL-PATTERN/pages/posts/index.js
@@ -8,7 +8,7 @@ const PostList = ({ posts }) => {
       {posts.map((post) => {
         return (
           <div key={post.id}>
-            <Link href={`/posts/${post.id}`} passHref /*if it has a child which is not an anchor tag*/ >
+            <Link href={`/posts/${post.id}`} passHref /* passHref, if it has a child which is not an anchor tag*/ >
               <p>Post ID: {post.id}</p>
               <h2>Title: {post.title}</h2>
             </Link>
@@ -27,9 +27,20 @@ export async function getStaticProps() {
   const response = await fetch(`http://jsonplaceholder.typicode.com/posts`)
   const data = await response.json();
 
+  //render all posts:
+  /*
   return {
     props: {
       posts: data,
+    }
+  };
+*/
+
+
+  //render only 3 posts
+  return {
+    props: {
+      posts: data.slice(0, 3),
     }
   };
 }
