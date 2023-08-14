add6df61811bd809e2e56286d53671a84aea4a9e
István Gergely Almási <almasi.istvan.gergely@gmail.com>
2023-08-14 21:49:56 +0200

    getServerSideProps function implemented + notes

---
+++ NEXT-SERVER-SIDE-RENDERING/pages/news/index.js
@@ -0,0 +1,28 @@
+const NewArticleList = ({ articles }) => {
+  return (
+    <>
+      <h1>List of News Articles</h1>
+      {articles.map(article => {
+        return (
+          <div key={article.title}>
+            <h2>Article ID: {article.id} | Title: {article.title} | Category: {article.category}</h2>
+            <p>Article Description: {article.description}</p>
+          </div>
+        )
+      })}
+    </>
+  )
+}
+
+export default NewArticleList;
+
+export async function getServerSideProps() {
+  const response = await fetch(`http://localhost:4000/news`)
+  const data = await response.json();
+
+  return {
+    props: {
+      articles: data,
+    }
+  }
+}
